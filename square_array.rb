e45e5e3e63718b65fc46f79b7da404ee01456125
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 02:46:41 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  roots = []
+  numbers.each { |num|
+    roots << num ** 2
+  }
+  roots
 end
+
+square_array([1, 2, 3])
+square_array([9, 10, 16, 25])
